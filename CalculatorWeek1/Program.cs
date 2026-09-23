8ea3a8a534849aeb4b4e4a720f7ff0fe33a834c5
Anjjellan <sam_santhakumar-a@ulster.ac.uk>
2026-09-23 21:40:00 +0300

    Implimented program code

--- CalculatorWeek1/Program.cs
+++ CalculatorWeek1/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in the first number, and then press Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type in the second number, and then press Enter");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result = firstNumber + secondNumber;
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
